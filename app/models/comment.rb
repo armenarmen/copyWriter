15aa8da191b148925a54c8f48e3eaad6b826920d
Armen <armen@Armens-MacBook-Pro.local>
2015-03-21 02:22:15 -0700

    fixed the span glitch adding custom validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,33 @@ class Comment < ActiveRecord::Base
   
   validates_numericality_of :char_began_at, :only_integer => true
   validates_numericality_of :char_ended_at, :only_integer => true
+
+  validate :no_range_overlap, on: :create
+
+  before_create :no_range_overlap
+
+  def no_range_overlap
+    # if char_began_at == 0
+    #   errors.add(:char_began_at, "can't be in the past")
+    # end
+
+    # comment in same range eliminate
+    comments = self.email.comments
+    if comments.where("char_began_at = #{char_began_at} AND char_ended_at = #{char_ended_at}").count >= 1
+      # true# We have another comment covering the exact same text range.  Allow this.
+    elsif comments.where("char_began_at < #{char_began_at} AND char_ended_at > #{char_began_at}").count >= 1
+      # Barf.
+      errors.add(:char_ended_at, "can't")
+      # false
+    elsif comments.where("char_began_at < #{char_ended_at}").count >= 1
+      # Barf.
+      errors.add(:char_began_at, "can't")
+      # false
+    end
+    true
+  end
+
+  def char_range
+    (self.char_began_at..self.char_ended_at).to_a
+  end
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -2,7 +2,6 @@ class Email < ActiveRecord::Base
   validates :user_id, presence: true
   validates :body, presence: true
   validates :subject, presence: true
-  # validates :comment, presence: true
 
   belongs_to :user, class_name: "User", foreign_key: "user_id"
   has_many :comments, dependent: :destroy
